307331162f4b091c421693e0d7a1aa539999167a
Cthax12 <Cthax12@gmail.com>
2023-01-27 16:23:58 -0600

    check if username has been taken

--- LoginLogout/sign.php
+++ LoginLogout/sign.php
@@ -1,3 +1,49 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+   include 'connect.php';
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+  //  $sql = "insert into registration (username, password) values('$username', '$password')";
+//
+  //  $result = mysqli_query($con, $sql);
+//
+  //  if ($result) {
+    //    echo "Data inserted successfully";
+    //}
+    //else {
+   //     die(mysqli_error($con));
+    //}
+//}
+
+    $sql = "Select * from registration where username='$username'";
+$result = mysqli_query($con, $sql);
+
+if ($result) {
+        $num = mysqli_num_rows($result);
+        if ($num > 0 ) {
+            echo "Username already taken";
+        }
+        else {
+            $sql = "insert into registration (username, password) values('$username', '$password')";
+
+            $result = mysqli_query($con, $sql);
+
+            if ($result) {
+                echo "Data inserted successfully";
+            }
+            else {
+                die(mysqli_error($con));
+            }
+}
+        }
+}
+
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -11,6 +57,21 @@
     <title>Registration Page</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <h1 class ="text-center mt-2">Registration</h1>
+    <div class ="container mt-5">
+    <form action="sign.php" method="post">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Username</label>
+    <input type="text" class="form-control" placeholder="Username" name="username">
+
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" placeholder="Password" name="password">
+  </div>
+
+  <button type="submit" class="btn btn-primary w-100">Submit</button>
+</form>
+</div>
   </body>
 </html>
